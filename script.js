446270b443f5d0ef5b743c0f677d05f5e868d4f5
Kamila <kamyyumy300@gmail.com>
2024-01-18 17:33:15 -0300

    adição de novos arquivos

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ function toggleMode() {
         img.setAttribute("src", "./assets/avatar-light.png")
     }else{
        //se tiver sem light mode, manter imagem atual
-        img.setAttribute("src", "./assets/avatar.png")
+        img.setAttribute("src", "./assets/Avatar.png")
     }
 }
