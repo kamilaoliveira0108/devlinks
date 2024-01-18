1d7eea0d5c0ce0b2779c2ce3e1f289ed909217c8
Kamila <kamyyumy300@gmail.com>
2024-01-18 15:48:47 -0300

    adição de novos arquivos

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function toggleMode() {
     const html = document.documentElement
-    html.classList.toggle('light')
+    html.classList.toggle("light")
     //pegar a tag img
     const img = document.querySelector('#profile img')
 
     //substituir a imagem
-    if(html.classList.contains('light')){
+    if(html.classList.contains("light")){
         //se tiver light mode, adicionar a imagem light
         img.setAttribute("src", "./assets/avatar-light.png")
     }else{
